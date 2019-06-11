9fa18d69ffbd7dba90c5ad65a46c69ff0b25f727
marci@PC-Win10 <androidu_sym@yahoo.com>
2019-06-12 01:04:18 +0300

    fixed rotations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const RADIANS = Math.PI / 180
 const TURN = RADIANS * 90
 const Vector3 = THREE.Vector3
+const Quaternion = THREE.Quaternion
 const ORIGIN = new Vector3(0, 0, 0)
 const RIGHT = new Vector3(1, 0, 0)
 const LEFT = new Vector3(-1, 0, 0)
@@ -8,9 +9,6 @@ const UP = new Vector3(0, 1, 0)
 const DOWN = new Vector3(0, -1, 0)
 const FRONT = new Vector3(0, 0, -1)
 const BACK = new Vector3(0, 0, 1)
-// const AXIS_X = new THREE.Group()
-// const AXIS_Y = new THREE.Group()
-// const AXIS_Z = new THREE.Group()
 const colors = {
     RED: 0xff0000,
     GREEN: 0x00aa00,
@@ -57,9 +55,6 @@ const container = document.getElementById('canvas')
 container.appendChild(renderer.domElement)
 window.addEventListener('resize', onWindowResize, false)
 
-
-
-let boxGeometry = new THREE.BoxGeometry(0.99, 0.99, 0.99)
 let planeGeometry = new THREE.PlaneGeometry(1, 1);
 
 { // make axis
@@ -73,42 +68,37 @@ camera.position.x = 2.5
 camera.position.y = 3
 camera.position.z = 5
 
-const Face = function (piece, position, direction, mat) {
-    this.position = position
-    this.direction = direction
-    const mesh = new THREE.Mesh(planeGeometry, mat)
-    mesh.position.x = this.position.x
-    mesh.position.y = this.position.y
-    mesh.position.z = this.position.z
-
-    if (this.direction.y != 0) {
-        mesh.rotation.x = TURN
-        mesh.position.y += mesh.position.y / 2.0
+class Face extends THREE.Mesh  {
+    constructor(piece, position, direction, mat) {
+        super(planeGeometry, mat)
+        this.position.x = position.x
+        this.position.y = position.y
+        this.position.z = position.z
+        this.direction = direction
+        
+        if (this.direction.y != 0) {
+            this.rotation.x = TURN
+            this.position.y += this.position.y / 2.0
+        }
+        if (this.direction.z != 0) {
+            this.position.z += this.position.z / 2.0
+        }
+        if (this.direction.x != 0) {
+            this.rotation.y = TURN
+            this.rotation.x = TURN
+            this.position.x += this.position.x / 2.0
+        }
     }
-    if (this.direction.z != 0) {
-        mesh.position.z += mesh.position.z / 2.0
-    }
-    if (this.direction.x != 0) {
-        mesh.rotation.y = TURN
-        mesh.rotation.x = TURN
-        mesh.position.x += mesh.position.x / 2.0
-    }
-
-    piece.obj.attach(mesh)
 }
-const Piece = function (position) {
-    // this.object = new THREE.Object3D();
-    // this.object.position = position    
-    this.originalPosition = position
-    this.positionBeforeAnimation = position
-    this.faces = []
-    this.obj = new THREE.Mesh(boxGeometry, materials.BLACK)
-    this.obj.position.x = position.x
-    this.obj.position.y = position.y
-    this.obj.position.z = position.z
-    // this.obj.scale.x = 0.99
-    // this.obj.scale.y = 0.99
-    // this.obj.scale.z = 0.99
+
+class Piece extends THREE.Mesh {
+    constructor(position) {
+        super(new THREE.BoxGeometry(0.99, 0.99, 0.99), materials.BLACK)
+        this.originalPosition = position.clone()
+        this.positionBeforeAnimation = position.clone()
+        this.dynamicPosition = position.clone()
+        this.geometry.translate(position.x, position.y, position.z)
+    }
 }
 
 // default cube configuration: white is up, red facing us
@@ -118,25 +108,25 @@ for (let x = -1; x <= 1; x++) {
         for (let z = -1; z <= 1; z++) {
             const piece = new Piece(new Vector3(x, y, z))
             if (x == -1) { // left
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(-1, 0, 0), materials.GREEN))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(-1, 0, 0), materials.GREEN))
             }
             if (x == 1) { // right
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(1, 0, 0), materials.BLUE))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(1, 0, 0), materials.BLUE))
             }
             if (z == 1) { // front
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(0, 0, 1), materials.RED))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(0, 0, 1), materials.RED))
             }
             if (z == -1) { // back
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(0, 0, -1), materials.ORANGE))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(0, 0, -1), materials.ORANGE))
             }
             if (y == 1) { // up
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(0, 1, 0), materials.WHITE))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(0, 1, 0), materials.WHITE))
             }
             if (y == -1) { // down
-                piece.faces.push(new Face(piece, new Vector3(x, y, z), new Vector3(0, -1, 0), materials.YELLOW))
+                piece.attach(new Face(piece, new Vector3(x, y, z), new Vector3(0, -1, 0), materials.YELLOW))
             }
             CubeGrid.push(piece)
-            scene.add(piece.obj)
+            scene.add(piece)
         }
     }
 }
@@ -185,40 +175,17 @@ orbit = new THREE.OrbitControls(camera, renderer.domElement)
 orbit.update()
 orbit.addEventListener('change', render)
 
-
-
 animate()
-
 
 function rotate(axis, offset, direction) {
     if (state.isMoving) return
 
     state.isMoving = true
-    state.axis = axis
+    state.axis = axis.clone()
+    state.axis.multiplyScalar(offset)
     state.t = 0.0
     state.direction = direction
     state.offset = offset
-
-    let pivot = new THREE.Object3D()
-    for (const piece of CubeGrid) {
-        if ((axis.x && piece.positionBeforeAnimation.x == axis.x) ||
-            (axis.y && piece.positionBeforeAnimation.y == axis.y) ||
-            (axis.z && piece.positionBeforeAnimation.z == axis.z)) {
-            // pivot.add(piece.obj)
-        }
-        if (axis.x && piece.obj.position.x == axis.x) {
-            // AXIS_X.add(piece.obj)
-            // scene.add(AXIS_X)
-        }
-        if (axis.y && piece.obj.position.y == axis.y) {
-            // AXIS_Y.add(piece.obj)
-            // scene.add(AXIS_Y)
-        }
-        if (axis.z && piece.obj.position.z == axis.z) {
-            // AXIS_Z.add(piece.obj)
-            // scene.add(AXIS_Z)            
-        }
-    }
 }
 
 function animate() {
@@ -231,58 +198,23 @@ function animate() {
             dt -= state.t - 1.0
         }
 
-        // if (state.axis == RIGHT) AXIS_X.rotateOnWorldAxis(axis, 90 * RADIANS * dt)
-        // else if (state.axis == UP) AXIS_Y.rotateOnWorldAxis(axis, 90 * RADIANS * dt)
-        // else if (state.axis == FRONT) AXIS_Z.rotateOnWorldAxis(axis, 90 * RADIANS * dt)
-
         for (const piece of CubeGrid) {
             if ((axis.x && piece.positionBeforeAnimation.x == axis.x) ||
                 (axis.y && piece.positionBeforeAnimation.y == axis.y) ||
                 (axis.z && piece.positionBeforeAnimation.z == axis.z)) {
-                    const mat4 = new THREE.Matrix4()
-                    const offset = new Vector3(
-                        piece.positionBeforeAnimation.x * 1,
-                        piece.positionBeforeAnimation.y * 1,
-                        piece.positionBeforeAnimation.z * 1
-                    )
-                    const q = new THREE.Quaternion();
-                    q.setFromRotationMatrix(piece.obj.matrix)
-                    q.rotateTowards(new THREE.Quaternion(axis.x,axis.y,axis.z, 1), TURN * dt)
-                    console.log(q)
-
-                    piece.obj.matrix.multiply(mat4.makeTranslation(-offset.x, -offset.y, -offset.z))
-                    piece.obj.matrix.multiply(mat4.makeRotationFromQuaternion(q))                    
-                    piece.obj.matrix.multiply(mat4.makeTranslation(offset.x, offset.y, offset.z))
-                    piece.obj.matrixAutoUpdate = false
-            }
-            if (axis.x && piece.obj.position.x == axis.x) {
-                
-            }
-            if (axis.y && piece.obj.position.y == axis.y) {
-                // AXIS_Y.add(piece.obj)
-                // scene.add(AXIS_Y)
-            }
-            if (axis.z && piece.obj.position.z == axis.z) {
-                // AXIS_Z.add(piece.obj)
-                // scene.add(AXIS_Z)            
+                    piece.rotateOnWorldAxis(axis, TURN * dt * -state.direction)
+                    piece.dynamicPosition.applyAxisAngle(axis, TURN * dt * -state.direction)
             }
         }
 
-        if (state.t >= 1) {
+        if (state.t >= 1 && state.isMoving) {
             state.axis = null
             state.isMoving = false
 
             for (const piece of CubeGrid) {
                 // correct rounding errors
-                piece.positionBeforeAnimation.x = Math.round(piece.obj.position.x)
-                piece.positionBeforeAnimation.y = Math.round(piece.obj.position.y)
-                piece.positionBeforeAnimation.z = Math.round(piece.obj.position.z)
-                // piece.obj.matrixAutoUpdate = false
-                // scene.add(piece.obj)
-                // piece.obj.attachTo(scene)
-                // piece.obj.position = piece.positionBeforeAnimation
+                piece.positionBeforeAnimation = piece.dynamicPosition.clone().round()
             }
-            // scene.remove(AXIS_X, AXIS_Y, AXIS_Z)            
         }
     }
 
@@ -300,39 +232,12 @@ function render() {
     renderer.render(scene, camera)
 }
 
-// obj - your object (THREE.Object3D or derived)
-// point - the point of rotation (THREE.Vector3)
-// axis - the axis of rotation (normalized THREE.Vector3)
-// theta - radian value of rotation
-// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
-// function rotateAroundPoint(obj, point, axis, theta, pointIsWorld) {
-//     // console.log("before rotation", obj.position)
-//     pointIsWorld = (pointIsWorld === undefined) ? false : pointIsWorld;
-
-//     if (pointIsWorld) {
-//         obj.parent.localToWorld(obj.position); // compensate for world coordinate
-//     }
-
-//     obj.position.sub(point); // remove the offset
-//     obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
-//     obj.position.add(point); // re-add the offset
-
-//     if (pointIsWorld) {
-//         obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
-//     }
-
-//     obj.rotateOnAxis(axis, theta); // rotate the OBJECT
-//     obj.updateMatrix()
-//     obj.updateMatrixWorld(true)
-//     // console.log("after rotation",obj.position)
-// }
-
 {
     const spriteMapCW = new THREE.TextureLoader().load("cw.png");
     const spriteMapCCW = new THREE.TextureLoader().load("ccw.png");
     const buttons = new THREE.Group()
     addButtons(new Vector3(3, 0, 0), 'right')
-    addButtons(new Vector3(-3, 0, 0)), 'left'
+    addButtons(new Vector3(-3, 0, 0), 'left')
     addButtons(new Vector3(0, 3, 0), 'top')
     addButtons(new Vector3(0, -3, 0), 'bottom')
     addButtons(new Vector3(0, 0, 3), 'back')
@@ -392,7 +297,6 @@ function render() {
     }
     document.onclick = function(e) {
         if(selectedObject) {
-            // console.log(selectedObject)
             switch(selectedObject.name) {
                 case 'right_cw':
                     rotate(RIGHT, 1, 1)
@@ -454,20 +358,3 @@ function makeLine(v1, v2, colorNormalized) {
     const line = new THREE.Line(geometry, mat)
     scene.add(line)
 }
-
-// THREE.Object3D.prototype.attachTo = function () {
-//     // adds this as a child of object, without changing the child's world transform
-//     let m = new THREE.Matrix4()
-//     return function attachTo(object) {
-//         object.updateWorldMatrix(true, false) // bubble up
-//         m.getInverse(object.matrixWorld)
-//         if (this.parent !== null) {
-//             this.parent.updateWorldMatrix(true, false) // bubble up
-//             m.multiply(this.parent.matrixWorld)
-//         }
-//         if (this.matrixAutoUpdate) this.updateMatrix()
-//         this.applyMatrix(m)
-//         this.updateWorldMatrix(false, false)
-//         object.add(this)
-//     }
-// }()
